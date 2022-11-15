7e180d7e8ff603a18f8e392a953c85e9c05d6783
tengo77 <kurmashvilitengo8@gmail.com>
2022-11-15 17:45:35 +0400

    active -router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelCardsComponent } from './components/hotel-cards/hotel-cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HotelCardsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
