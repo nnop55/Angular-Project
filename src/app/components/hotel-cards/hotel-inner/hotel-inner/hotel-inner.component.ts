5d56e28483edc4c49c7dfd65fcce27bbaf70415a
Tako Barkalaia <114744524+TakoBarkalaia@users.noreply.github.com>
2022-12-19 17:05:20 +0400

    gavaswore mgoni

--- src/app/components/hotel-cards/hotel-inner/hotel-inner/hotel-inner.component.ts
+++ src/app/components/hotel-cards/hotel-inner/hotel-inner/hotel-inner.component.ts
@@ -12,6 +12,8 @@ import { HotelsHttpService } from 'src/app/services/hotels-http.service';
 export class HotelInnerComponent implements OnInit {
 
   currentHotel: any;
+  placeOffers:any;
+  HotelCategories:any;
   
 
   constructor(private http: HotelsHttpService, private activatedRoute: ActivatedRoute, public dialog:MatDialog) { }
@@ -20,19 +22,44 @@ export class HotelInnerComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params: any) => {
       let id = params['id'];
-<<<<<<< Updated upstream
-      console.log(params);
 
-=======
+      console.log(params);
+    
       console.log(id);
-      
->>>>>>> Stashed changes
+
       this.http.getHotelById(id).subscribe(res => {
         this.currentHotel = res;
+        this.placeOffers = this.currentHotel.placeOffers;
+        this.HotelCategories = this.currentHotel.categories;
         console.log(this.currentHotel);
       })
     })
   }
+  previousImage(item: any) {
+    item.imgIndex = item.imgIndex ? item.imgIndex : 0;
+
+    if (item.imgIndex != 0) {
+      item.imgIndex--;
+    } else {
+      item.imgIndex = item.images.length - 1
+    }
+  }
+
+  nextImage(item: any) {
+    item.imgIndex = item.imgIndex ? item.imgIndex : 0;
+    item.imgIndex++;
+
+    if (item.imgIndex > (item.images.length - 1)) {
+      item.imgIndex = 0;
+    }
+  }
+
+  updateUrl(event: Event) {
+    let ev = event.target as HTMLImageElement;
+    ev.src = "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png";
+  }
+
 
 }
+
 
